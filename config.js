dbe408118716a0565409e82af85be1c3ebc94191
AndriiKot <AndriiKot@users.noreply.github.com>
2025-10-18 19:44:01 +0200

    feat(hash): add hashing for honor,authored, position, ranks, uniques

--- config.js
+++ config.js
@@ -1,10 +1,4 @@
 export const USER_NAME = "AndriiKot";
 export const USER_POSITION = "leaderboardPosition";
-export const USER_FIELDS = [
-  "id",
-  "username",
-  "honor",
-  "ranks",
-  "codeChallenges",
-];
+export const USER_FIELDS_RANKS = ["ranks"];
 export const USER_CACHE_DIR = `cache/codewars/`;
--- services/hash/storage.js
+++ services/hash/storage.js
@@ -36,3 +36,60 @@ export const loadPosition = () => {
   if (!fs.existsSync(positionFile)) return null;
   return JSON.parse(fs.readFileSync(positionFile, "utf8")).leaderboardPosition;
 };
+
+export const saveAuthoredKatas = (katas) => {
+  if (!fs.existsSync(USER_CACHE_DIR)) {
+    fs.mkdirSync(USER_CACHE_DIR, { recursive: true });
+  }
+  fs.writeFileSync(
+    path.join(USER_CACHE_DIR, `user-authored-katas-${USER_NAME}.json`),
+    JSON.stringify({ totalAuthored: katas }),
+    "utf8"
+  );
+};
+
+export const loadAuthoredKatas = () => {
+  const filePath = path.join(
+    USER_CACHE_DIR,
+    `user-authored-katas-${USER_NAME}.json`
+  );
+  if (!fs.existsSync(filePath)) return null;
+  return JSON.parse(fs.readFileSync(filePath, "utf8")).totalAuthored;
+};
+
+export const saveHonor = (honor) => {
+  if (!fs.existsSync(USER_CACHE_DIR)) {
+    fs.mkdirSync(USER_CACHE_DIR, { recursive: true });
+  }
+  fs.writeFileSync(
+    path.join(USER_CACHE_DIR, `user-honor-${USER_NAME}.json`),
+    JSON.stringify({ honor }),
+    "utf8"
+  );
+};
+
+export const loadHonor = () => {
+  const filePath = path.join(USER_CACHE_DIR, `user-honor-${USER_NAME}.json`);
+  if (!fs.existsSync(filePath)) return null;
+  return JSON.parse(fs.readFileSync(filePath, "utf8")).honor;
+};
+
+export const saveTotalUniquesKatas = (katas) => {
+  if (!fs.existsSync(USER_CACHE_DIR)) {
+    fs.mkdirSync(USER_CACHE_DIR, { recursive: true });
+  }
+
+  fs.writeFileSync(
+    path.join(USER_CACHE_DIR, `user-uniques-katas-${USER_NAME}.json`),
+    JSON.stringify({ totalCompleted: katas }),
+    "utf8"
+  );
+};
+export const loadTotalUniquesKatas = () => {
+  const filePath = path.join(
+    USER_CACHE_DIR,
+    `user-uniques-katas-${USER_NAME}.json`
+  );
+  if (!fs.existsSync(filePath)) return null;
+  return JSON.parse(fs.readFileSync(filePath, "utf8")).totalCompleted;
+};
