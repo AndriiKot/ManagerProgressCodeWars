b7fd2991b29f5207a759fc061ed6573b38284e44
AndriiKot <AndriiKot@users.noreply.github.com>
2025-10-30 16:16:16 +0100

    chore(config):  update config.js

--- backend/services/storage/index.js
+++ backend/services/storage/index.js
@@ -1,19 +1,26 @@
+import { join } from 'node:path';
+import { CACHE_DIR_CODEWARS, DATA_DIR_CODEWARS } from '#config';
 import { loadJSONAsObject, writeObjectToJSON } from './utils/index.js';
 
 export const Storage = {
   async update({ user, data }) {
+    const userCache = join(CACHE_DIR_CODEWARS, user);
+    const userData =  join(DATA_DIR_CODEWARS, user);
     console.log('update Storage');
     console.log(user);
     console.log(data);
+    console.log(userCache);
+    console.log(userData); 
+     
   },
 
   async load(pathFile) {
-    console.log('test load fn');
+    console.log('load json as object');
     loadJSONAsObject(pathFile); 
   },
 
   async write({ filePath, data }) {
-    console.log('test write fn');
+    console.log('write Object to json');
     writeObjectToJSON({ filePath, dataObject: data });
   },
 
--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 export const USER_NAME = "AndriiKot";
 export const CACHE_DIR = 'backend/cache/';
 export const CACHE_DIR_CODEWARS = 'cache/codewars/';
+export const DATA_DIR_CODEWARS = 'backend/data/';
