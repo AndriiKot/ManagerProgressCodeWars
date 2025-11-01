fc5d20cc7517b1722b0b156382a8887fce54e862
AndriiKot <AndriiKot@users.noreply.github.com>
2025-11-01 16:14:40 +0100

    fix(config):  bag fix path config

--- config.js
+++ config.js
@@ -1,4 +1,9 @@
+import { fileURLToPath } from "node:url";
+import { dirname, join } from "node:path";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 export const USER_NAME = "AndriiKot";
-export const CACHE_DIR = 'backend/cache/';
-export const CACHE_DIR_CODEWARS = 'cache/codewars/';
-export const DATA_DIR_CODEWARS = 'backend/data/';
+export const CACHE_DIR = join(__dirname, 'backend', 'cache');
+export const CACHE_DIR_CODEWARS = join(__dirname, "backend", "cache", "codewars");
+export const DATA_DIR_CODEWARS = join(__dirname, "backend", "data", "codewars");
