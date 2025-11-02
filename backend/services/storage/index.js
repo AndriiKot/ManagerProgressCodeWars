ebc6fec0554a7224a027d936e7afe2dab1a7c3b0
AndriiKot <AndriiKot@users.noreply.github.com>
2025-11-02 08:36:38 +0100

    feat(storage):  update delta data level 1 and level 2

--- backend/services/storage/index.js
+++ backend/services/storage/index.js
@@ -10,25 +10,52 @@ export const Storage = {
     const userCache = join(CACHE_DIR_CODEWARS, user);
     const userData =  join(DATA_DIR_CODEWARS, user);
     const delta = {};
-
-    const newUserProfileHash = generateCryptoHash(data);
-    const oldUserProfileHash = await this.load(join(userCache, 'userProfile.hash.json'));
-
-    if (newUserProfileHash === oldUserProfileHash) {
-      return { change: false, delta };
+    const deltaHash = { fullHash: '', fields: {}, "hash-fields": {} };
+    const updateResult = {
+      data: { change: false, delta },
+      hash: { change: false, deltaHash },
     };
 
+    const newUserProfileHash = generateCryptoHash(data);
+    const oldUserProfileHash  = await this.load(join(userCache, 'userProfile.hash.json'));
+    
+    // level 1 fulHash userProfile comparable 
+    const { fullHash: oldHash } = oldUserProfileHash;
+    if (newUserProfileHash === oldHash) {
+      return updateResult;
+    } else {
+      updateResult.hash.change = true;
+      updateResult.data.change = true;
+      deltaHash.fullHash = newUserProfileHash;
+    };
+    
+    
+    // level 2 simple fileds comparable don`t use crypto hash
     const { fields , fieldsUseHash } = CodewarsProfileCacheSchemas;
 
     fields.reduce((acc, curr) => {
       const newValue = getValueByPath(data, curr);
       const oldValue = oldUserProfileHash.fields[curr];
       if (newValue !== oldValue) {
-        acc[curr] = newValue;
+        delta[curr] = newValue;
+        deltaHash.fields[curr] = newValue;
+        updateResult.hash.change = true;
       }
       return acc;
     }, delta);
-    console.dir(delta);
+    console.log(updateResult);
+    
+    // level 3 RanksCryptoHash comprable 
+    const { ranks: newRanks } = data;
+    const newRanksHash = generateCryptoHash(newRanks);
+    const { "hash-fields": { ranks: oldRanksHash }  } = oldUserProfileHash;
+
+    if (newRanksHash === oldRanksHash) {
+      return updateResult;
+    };
+        
+    
+        
   },
 
   async load(pathFile) {
