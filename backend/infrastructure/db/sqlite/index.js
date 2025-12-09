27e7f48742633df1a1d693fbbc0e9c5300fa2c85
AndriiKot <AndriiKot@users.noreply.github.com>
2025-12-09 17:08:10 +0100

    chore(db):  delete utils folder

--- backend/infrastructure/db/sqlite/index.js
+++ backend/infrastructure/db/sqlite/index.js
@@ -1,4 +1,3 @@
-export * from './utils/index.js';
 export * from './repositories/index.js';
 export * from './schema/index.js';
 export * from './seed/index.js';
--- backend/infrastructure/db/sqlite/utils/index.js
+++
@@ -1,1 +0,0 @@
-export * from './prepareDatabase.js';
