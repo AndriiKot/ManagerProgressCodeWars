0c731c8683b1e77580b23039bf7b278d5c8787dc
AndriiKot <AndriiKot@users.noreply.github.com>
2025-12-10 19:30:02 +0100

    feat(config):  update config

--- config.js
+++ config.js
@@ -3,31 +3,20 @@ import { dirname, join } from 'node:path';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
+const ROOT_DIR = __dirname;            
+const BACKEND_DIR = join(ROOT_DIR, 'backend');
+const INFRA_DIR = join(BACKEND_DIR, 'infrastructure');
+
 export const USER_NAME = 'AndriiKot';
+export const FRIENDS = [...new Set(['Krillan'])];
+export const MAX_USERS = 10;
 
-export const FRIENDS = ([...new Set(['Krillan'])]);
+export const CACHE_DIR = join(INFRA_DIR, 'cache');
+export const CACHE_DIR_CODEWARS = join(CACHE_DIR, 'codewars');
 
-export const MAX_USERS = 10; 
+export const DATA_DIR_CODEWARS = join(INFRA_DIR, 'data', 'codewars');
+export const CHALLENGES_DIR = join(DATA_DIR_CODEWARS, 'Challenges');
 
-export const CACHE_DIR = join(__dirname, 'infrastructure', 'backend', 'cache');
-export const CACHE_DIR_CODEWARS = join(
-  __dirname,
-  'backend',
-  'infrastructure',
-  'cache',
-  'codewars',
-);
-
-export const DATA_DIR_CODEWARS = join(__dirname, 'backend', 'infrastructure', 'data', 'codewars');
-export const CHALLENGES_DIR = join(
-  __dirname,
-  'backend',
-  'infrastructure',
-  'data',
-  'codewars',
-  'Challenges'
-);
-
-export const DB_SCHEMAS = join(__dirname, 'backend', 'infrastructure', 'db', 'sqlite', 'schema', 'schema.sql');
-export const DB_FILE = join(__dirname, 'backend', 'infrastructure', 'db', 'sqlite', 'database.sqlite');
-
+export const DB_BASE = join(INFRA_DIR, 'db', 'sqlite');
+export const DB_SCHEMAS = join(DB_BASE, 'schema', 'schema.sql');
+export const DB_FILE = join(DB_BASE, 'database.sqlite');
