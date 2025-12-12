de4013b4d177b7847c1fe7e5b5bf602f7eb045d9
AndriiKot <AndriiKot@users.noreply.github.com>
2025-12-12 13:22:07 +0100

    feate(globalCache):  add users-slug and users-codewars-id cache

--- backend/infrastructure/cache/codewars/buildGlobalCache.js
+++ backend/infrastructure/cache/codewars/buildGlobalCache.js
@@ -3,15 +3,20 @@
 import { loadUsersCache } from '#cache';
 import { sqlite } from '#db';
 
-const { selectAllChallengeIds } = sqlite;
+const { selectAllChallengeIds, selectAllUsersSlugsAndIds } = sqlite;
 
 export const buildGlobalCache = async (db) => {
-  const { 0: usersCache, 1: challengeIds } = await Promise.all([
+  const { 0: usersCache, 1: challengeIds, 2: usersSlugAndIds } = await Promise.all([
     loadUsersCache(),
     Promise.resolve(selectAllChallengeIds(db)),
+    Promise.resolve(selectAllUsersSlugsAndIds(db)),
   ]);
 
+  const { slugs, ids } = usersSlugAndIds;
+
   return {
+    usersSlug: slugs,
+    usersIds: ids,
     users:  usersCache,
     challenges: challengeIds,
   };
