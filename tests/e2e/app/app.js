cf76825c164e7c39a79a8c49a48c7b06b2bdc427
AndriiKot <AndriiKot@users.noreply.github.com>
2025-12-25 19:25:26 +0100

    chore(data):  update raw data

--- tests/e2e/app/app.js
+++ tests/e2e/app/app.js
@@ -12,7 +12,7 @@ import { getUserProfile, createUserService } from '#services';
 console.log('⚠️ Running in test mode, database: test_database.sqlite');
 
 (async () => {
-  const usersToCheck = deepFreezeArray([USER_NAME, ...FRIENDS, 'Voile']);
+  const usersToCheck = deepFreezeArray([USER_NAME, ...FRIENDS]);
   checkUsersLimit(usersToCheck);
 
   const db = bootstrapTestDatabase('test_database.sqlite');
@@ -25,7 +25,7 @@ console.log('⚠️ Running in test mode, database: test_database.sqlite');
   const { saveFullUser, saveAuthoredChallenges, savePages } = userService;
 
   for (const username of usersToCheck) {
-      const { success, data: profileData, error } = await withTimeout(
+      const { success, data: profileData, error, isValid, validationErrors } = await withTimeout(
         () => getUserProfile(username),
         7000,
         1
@@ -35,12 +35,20 @@ console.log('⚠️ Running in test mode, database: test_database.sqlite');
         console.warn(`Failed to fetch profile for ${username}:`, error);
         continue;
       }
+ 
+      if (!isValid) {
+        console.warn(`Respons data is not valid for ${username}: `, validationErrors);
+        continue;
+      }
 
       console.log(`User profile ${username} data is valid!`);
 
-      const userId = await saveFullUser(profileData);
-      await saveAuthoredChallenges(userId, { username });
-      await savePages(userId, { username }); 
-          
+      try {
+        const userId = await saveFullUser(profileData);
+        await saveAuthoredChallenges(userId, { username });
+        await savePages(userId, { username });
+      } catch (err) {
+        console.error(`Failed to save data for ${username}`, err);
+      }           
   }
 })();
