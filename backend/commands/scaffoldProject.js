25f95699ae938b56a4e4882958667f98012894a2
AndriiKot <AndriiKot@users.noreply.github.com>
2025-12-30 18:01:33 +0100

    chore(cli): setup command architecture for init

---
+++ backend/commands/scaffoldProject.js
@@ -0,0 +1,10 @@
+'use strict';
+
+export const scaffoldProject = async() => {
+  const root = process.cwd();
+
+  console.log(root);
+
+  console.log("✅ Codewars bot initialized");
+}
+
--- bin/cli.js
+++ bin/cli.js
@@ -1,3 +1,15 @@
 #!/usr/bin/env node
-import '../backend/index.js';
 
+import { scaffoldProject as init } from "#commands";
+
+const [, , command] = process.argv;
+
+switch (command) {
+  case "init":
+    await init();
+    break;
+
+  default:
+    console.log("Usage:");
+    console.log("  codewars_bot init");
+}
